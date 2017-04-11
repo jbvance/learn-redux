765650eb2b9f7569b0ed5a97cd4e9c1f69c3db79
Jason Vance <jbvance@gmail.com>
2017-04-11 16:17:02 -0500

    Add photogrid and single components and add initial routing

---
+++ client/components/Single.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+ const Single = React.createClass({
+  render (){
+    return (
+      <div className="single-photo">
+        I am the single
+      </div>
+    );
+  }
+});
+
+export default Single;
